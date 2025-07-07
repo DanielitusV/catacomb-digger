08a9290524033ce03987e4e719d574057075d11b
Daniel Virreira <virreira.daniel@gmail.com>
2025-07-07 15:36:53 -0400

    Corregir main sin estetica

--- src/main/menu/MainMenu.java
+++ src/main/menu/MainMenu.java
@@ -1,13 +1,20 @@
 package main.menu;
 
 import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 import javax.swing.SwingUtilities;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
+
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.Insets;
 
 import main.interfaces.MenuInterface;
 
@@ -20,18 +27,32 @@ public class MainMenu {
         mainFrame = new JFrame("Catacomb Digger - Main Menu");
         mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         mainFrame.setSize(800, 600);
-    
+        mainFrame.setLocationRelativeTo(null);
+
         actions.put("New Game", new NewGameAction());
         actions.put("Load Game", new LoadGameAction());
         actions.put("Quit", new QuitGameAction());
 
         JPanel mainPanel = new JPanel();
         mainPanel.setLayout(new GridBagLayout());
-        
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = GridBagConstraints.RELATIVE;
+        gbc.insets = new Insets(15, 0, 15, 0);
+
+        JLabel title = new JLabel("Catacomb Digger");
+        title.setFont(new Font("Times New Roman", Font.BOLD, 36));
+        title.setHorizontalAlignment(SwingConstants.CENTER);
+
+        gbc.anchor = GridBagConstraints.CENTER;
+        mainPanel.add(title, gbc);
+
         for (Map.Entry<String, MenuInterface> entry : actions.entrySet()) {
             JButton button = new JButton(entry.getKey());
+            button.setFont(new Font("Arial", Font.BOLD, 22));
+            button.setPreferredSize(new Dimension(300, 60));
             button.addActionListener(e -> entry.getValue().execute());
-            mainPanel.add(button);
+            mainPanel.add(button, gbc);
         }
 
         mainFrame.add(mainPanel);
